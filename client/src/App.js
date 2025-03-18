15ea40fab4491c0aa0641c630bd6b1521163a2e6
davy233 <deivimauriciodavid@gmail.com>
2025-03-17 22:38:31 -0600

    modificaciones en el front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="datos">
+        <label>Nombre: <input type="text"/></label><br/>
+        <label>Edad: <input type="text"/></label><br/>
+        <label>Pais: <input type="text"/></label><br/>
+        <label>Cargo: <input type="text"/></label><br/>
+        <label>Anios: <input type="text"/></label><br/>
+      </div>
     </div>
   );
 }
