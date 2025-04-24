5528a817960ad238e8af0132436e8afe3135f401
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2025-04-24 11:13:30 -0600

    TablaTipoPrenda

---
+++ client/src/pages/Mantenimiento.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>Aqui va el contenido de mantenimiento</h1>;
+  }
---
+++ client/src/pages/Pedidos.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>aqui va el contenido de pedidos</h1>;
+  }
